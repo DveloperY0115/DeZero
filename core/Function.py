02d85f51f771d09b175f547e30ced53b9c39b37a
SeungWoo Yoo <dreamy1534@gmail.com>
2020-12-18 18:37:26 +0900

    feat(main): Implement backward propagation
    feat(main): Implement backward propagation

--- core/Function.py
+++ core/Function.py
@@ -7,7 +7,9 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
         self.input = input
+        self.output = output
         return output
 
     def forward(self, x):
--- core/Variable.py
+++ core/Variable.py
@@ -2,3 +2,14 @@ class Variable:
     def __init__(self, data):
         self.data = data
         self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()

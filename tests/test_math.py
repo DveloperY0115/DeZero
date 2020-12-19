e03067a78c09910d2d51c7cc44fa2906dfce5ab4
Yoo SeungWoo <dreamy1534@gmail.com>
2020-12-19 16:51:02 +0900

    feat(main): Add test case for 'exp'

--- tests/test_math.py
+++ tests/test_math.py
@@ -5,8 +5,16 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import unittest
 import numpy as np
 from core.Variable import Variable
-from core.Function import square, exp
+from core.Function import add, square, exp
 from core.Diff import numerical_diff
+
+class AddTest(unittest.TestCase):
+    def test_forward(self):
+        x0 = Variable(np.array(2.0))
+        x1 = Variable(np.array(5.0))
+        y = add(x0, x1)
+        expected = np.array(7.0)
+        self.assertEqual(y.data, expected)
 
 
 class SquareTest(unittest.TestCase):
@@ -28,6 +36,16 @@ class SquareTest(unittest.TestCase):
         y = square(x)
         y.backward()
         num_grad = numerical_diff(square, x)
+        flg = np.allclose(x.grad, num_grad)
+        self.assertTrue(flg)
+
+
+class ExpTest(unittest.TestCase):
+    def test_gradient_check(self):
+        x = Variable(np.random.rand(1))
+        y = exp(x)
+        y.backward()
+        num_grad = numerical_diff(exp, x)
         flg = np.allclose(x.grad, num_grad)
         self.assertTrue(flg)
 

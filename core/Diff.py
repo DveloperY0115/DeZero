ee58c725257b80f1d65f4210bc115a0167d2d825
Yoo SeungWoo <dreamy1534@gmail.com>
2020-12-19 13:26:43 +0900

    feat(main): Add comment to 'numerical_diff'

--- core/Diff.py
+++ core/Diff.py
@@ -3,6 +3,13 @@ from .Function import Function
 
 
 def numerical_diff(f, x, eps=1e-4):
+    """
+    Calculate the differential coefficient of function 'f' at point 'x'
+    :param f: Instance of function classes
+    :param x: Instance of numpy array
+    :param eps: Rate of change of 'x', default is 1e-4
+    :return: df/dx at given point 'x'
+    """
     x0 = Variable(x.data - eps)
     x1 = Variable(x.data + eps)
     y0 = f(x0)
